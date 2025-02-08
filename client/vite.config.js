935714e0d33544aba21e468129354f1b0d398314
samir717le <lestedit717@gmail.com>
2025-02-08 05:20:09 +0000

    H.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite';
-   import react from '@vitejs/plugin-react';
-
-   export default defineConfig({
-     plugins: [react()],
+import { defineConfig
+} from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from "path";
+export default defineConfig({
+     plugins: [react(), tailwindcss()],
      server: {
        host: '0.0.0.0', // Listens on all network interfaces
        allowedHosts: [
-         '5173-crinix-labs-s7l-client-ii2g2i9g8q.app.codeanywhere.com' // Add your CodeAnywhere host
-       ]
-     }
-   }); 
+         '5173-crinix-labs-s7l-client-ii2g2i9g8q.app.codeanywhere.com', // Add your CodeAnywhere host
+         '5173-crinix-labs-s7l-client-ii2g2i9g8q.app.codeanywhere.com',
+         'localhost',
+         'wanted-man-sterling.ngrok-free.app'
+    ]
+  },
+     resolve: {
+      alias: {
+      "@": path.resolve(__dirname,
+      "./src"),
+    },
+  }
+}); 
