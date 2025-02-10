6b905a46335c9be5c0295dd4bd21e5151083d210
SAMIR 717 LE <158018273+samir717le@users.noreply.github.com>
2025-02-10 13:21:08 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import "./App.css"
-
-function Home() {
-  const [response, setResponse] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("/api")
-      .then((res) => {
-        setResponse(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  return (
-    <>
-      <h1>Home</h1>
-      <h3 className="bg-dark">res: {response.message}</h3>
-    </>
-  );
-}
-
-function About() {
-  return <h1>About</h1>;
-}
-
-function App() {
-  return (
-    <Router>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
-  );
-}
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
@@ -55,6 +10,5 @@ const App = () => (
     </Routes>
   </Router>
 );
->>>>>>> 509f58bcc18da08c4bd5e06ca912f64c2d0f2153
 
 export default App;
