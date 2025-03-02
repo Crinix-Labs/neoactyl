4a588f0da8bea7e0bfdbfe92c23fe1ca20a188a4
SAMIR 717 LE <158018273+samir717le@users.noreply.github.com>
2025-03-02 18:54:48 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Loginform from "./pages/Loginform";
+import Nopage from "./pages/404";
 
 function App() {
   return (
-    <div className="flex justify-center items-center w-100vw h-100vh flex-col">
-  <a href="/" className="link">Main (Done)</a>
-  <a href="/auth" className="link">login (Done)</a>
-  <a href="/dashboard" className="link">User Dashboard (Pending) </a>
-  <a href="/admin/config" className="link">Admin Dash (Pending)</a>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Loginform />} />
+        <Route path="*" element={<Nopage />} />
+      </Routes>
+    </Router>
   );
 }
 
