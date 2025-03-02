cd48d7a4312caff10f40e213242ef533d161053c
SAMIR 717 LE <158018273+samir717le@users.noreply.github.com>
2025-03-02 22:43:15 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Loginform from "./pages/Loginform";
 import Nopage from "./pages/404";
+import Dashboard from "./pages/Dashboard"; // Example protected page
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route index element={<Loginform />} />
+
+        {/* Protected Routes */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
         <Route path="*" element={<Nopage />} />
       </Routes>
     </Router>
